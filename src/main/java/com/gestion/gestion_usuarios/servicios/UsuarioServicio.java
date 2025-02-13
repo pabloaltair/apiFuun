5d9169870fd9dcb07a12dccd2b7c662d07cec994
pabloaltair <csi24-prosdez@colegioaltair.net>
2025-02-13 12:32:13 +0100

    emailintentoImplementacion

--- src/main/java/com/gestion/gestion_usuarios/servicios/UsuarioServicio.java
+++ src/main/java/com/gestion/gestion_usuarios/servicios/UsuarioServicio.java
@@ -1,9 +1,13 @@
 package com.gestion.gestion_usuarios.servicios;
 
 import java.util.Optional;
+import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +15,9 @@ import org.springframework.transaction.annotation.Transactional;
 import com.gestion.gestion_usuarios.daos.UsuarioDao;
 import com.gestion.gestion_usuarios.dtos.RegistroUsuarioDto;
 import com.gestion.gestion_usuarios.repositorios.UsuarioRepository;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 @Service
 public class UsuarioServicio {
@@ -176,5 +183,41 @@ public class UsuarioServicio {
         }
         return false; // Retorna false si el usuario no existe
     }
+	
+	// Método para enviar correos
+	public void enviarCorreo(String correoDestinatario, String asunto, String mensaje) throws MessagingException {
+	    JavaMailSender mailSender = configurarServidorSMTP();
+	    MimeMessage mimeMessage = mailSender.createMimeMessage();
+	    MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+	    helper.setTo(correoDestinatario);
+	    helper.setSubject(asunto);
+	    helper.setText(mensaje, false); // false = texto plano
+	    helper.setFrom("bonsaisur@gmail.com");
+
+	    mailSender.send(mimeMessage);
+	    System.out.println("[Correo enviado a " + correoDestinatario + "]");
+	}
+
+	// Configuración del servidor SMTP (solo si no inyectas JavaMailSender desde Spring)
+	private JavaMailSender configurarServidorSMTP() {
+	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+	    mailSender.setHost("smtp.gmail.com");
+	    mailSender.setPort(587);
+	    mailSender.setUsername("bonsaisur@gmail.com");
+	    mailSender.setPassword("msprjeksnbhekmjc");
+
+	    Properties props = mailSender.getJavaMailProperties();
+	    props.put("mail.transport.protocol", "smtp");
+	    props.put("mail.smtp.auth", "true");
+	    props.put("mail.smtp.starttls.enable", "true");
+	    props.put("mail.smtp.starttls.required", "true");
+
+	    mailSender.setJavaMailProperties(props);
+	    return mailSender;
+	}
+
+	
+	
 
 }
