c7d5c06584070083635e959a0042db7d5aa9c83e
pabloaltair <csi24-prosdez@colegioaltair.net>
2025-02-13 12:41:24 +0100

    emailIntentoborrado

--- src/main/java/com/gestion/gestion_usuarios/servicios/UsuarioServicio.java
+++ src/main/java/com/gestion/gestion_usuarios/servicios/UsuarioServicio.java
@@ -5,9 +5,6 @@ import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -183,41 +180,5 @@ public class UsuarioServicio {
         }
         return false; // Retorna false si el usuario no existe
     }
-	
-	// Método para enviar correos
-	public void enviarCorreo(String correoDestinatario, String asunto, String mensaje) throws MessagingException {
-	    JavaMailSender mailSender = configurarServidorSMTP();
-	    MimeMessage mimeMessage = mailSender.createMimeMessage();
-	    MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-
-	    helper.setTo(correoDestinatario);
-	    helper.setSubject(asunto);
-	    helper.setText(mensaje, false); // false = texto plano
-	    helper.setFrom("bonsaisur@gmail.com");
-
-	    mailSender.send(mimeMessage);
-	    System.out.println("[Correo enviado a " + correoDestinatario + "]");
-	}
-
-	// Configuración del servidor SMTP (solo si no inyectas JavaMailSender desde Spring)
-	private JavaMailSender configurarServidorSMTP() {
-	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-	    mailSender.setHost("smtp.gmail.com");
-	    mailSender.setPort(587);
-	    mailSender.setUsername("bonsaisur@gmail.com");
-	    mailSender.setPassword("msprjeksnbhekmjc");
-
-	    Properties props = mailSender.getJavaMailProperties();
-	    props.put("mail.transport.protocol", "smtp");
-	    props.put("mail.smtp.auth", "true");
-	    props.put("mail.smtp.starttls.enable", "true");
-	    props.put("mail.smtp.starttls.required", "true");
-
-	    mailSender.setJavaMailProperties(props);
-	    return mailSender;
-	}
-
-	
-	
 
 }
